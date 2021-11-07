655d2f86c6e95fb1a44cb05a38cad6ae683a5996
camelcrush <dkxm0927@naver.com>
2021-11-07 14:09:06 +0900

    Writing Object: ContextAPI for Realm & navigation goBack()

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Realm from "realm";
 import React, { useState } from "react";
 import Navigator from "./navigator";
 import AppLoading from "expo-app-loading";
+import { DBContext } from "./context";
 
 const FeelingSchema = {
   name: "Feeling",
@@ -16,11 +17,13 @@ const FeelingSchema = {
 
 export default function App() {
   const [ready, setReady] = useState(false);
+  const [realm, setRealm] = useState(null);
   const startLoading = async () => {
-    const realm = await Realm.open({
+    const connection = await Realm.open({
       path: "diaryDB",
       schema: [FeelingSchema],
     });
+    setRealm(connection);
   };
   const onFinish = () => setReady(true);
   if (!ready) {
@@ -33,8 +36,10 @@ export default function App() {
     );
   }
   return (
-    <NavigationContainer>
-      <Navigator />
-    </NavigationContainer>
+    <DBContext.Provider value={realm}>
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    </DBContext.Provider>
   );
 }
--- screens/Write.js
+++ screens/Write.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Alert } from "react-native";
 import styled from "styled-components/native";
 import colors from "../colors";
+import { useDB } from "../context";
 
 const View = styled.View`
   flex: 1;
@@ -63,7 +64,8 @@ const EmotionText = styled.Text`
 
 const emotions = ["🤯", "🥲", "🤬", "🤗", "🥰", "😊", "🤩"];
 
-const Write = () => {
+const Write = ({ navigation: { goBack } }) => {
+  const realm = useDB();
   const [selectedEmotion, setEmotion] = useState(null);
   const [feelings, setFeelings] = useState("");
   const onChangeText = (text) => setFeelings(text);
@@ -72,6 +74,15 @@ const Write = () => {
     if (feelings === "" || selectedEmotion === null) {
       return Alert.alert("Please complete form");
     }
+    realm.write(() => {
+      realm.create("Feeling", {
+        _id: Date.now(),
+        emotion: selectedEmotion,
+        message: feelings,
+      });
+    });
+    console.log(selectedEmotion, feelings);
+    goBack();
   };
   return (
     <View>
